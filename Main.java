22bb1aa31cdb9cbae2abb6202157475e32d5836e
MocusTocus <ivanov.maxim0@yandex.ru>
2024-11-28 14:32:16 +0300

    Main updated 4

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("haloy1");
         System.out.println("haloy2");
         System.out.println("haloy3");
+        System.out.println("haloy4");
     }
 }
